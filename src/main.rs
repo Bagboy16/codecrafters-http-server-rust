aa7f454c7adae436a6a9c58b5638c794f8cace23
Isaac Galviz <thebag16boy@gmail.com>
2024-03-18 18:18:00 -0400

    first test

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
 // Uncomment this block to pass the first stage
-// use std::net::TcpListener;
+use std::net::TcpListener;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
-    // let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+    
+    // Create a listener bound to 
+    // unwrap() is used to panic if the listener can't be created
+    // panic! is a macro that prints an error message and exits the program
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+
+    // listener.incoming() returns an iterator over incoming connections
+    
+    for stream in listener.incoming() {
+        // match is used to handle the Result returned by incoming()
+        // if the Result is Ok, the stream is printed 
+        // if the Result is Err, the error is printed
+        match stream {
+            Ok(_stream) => {
+                println!("accepted new connection");
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
 }
